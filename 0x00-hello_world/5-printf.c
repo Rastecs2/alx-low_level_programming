28208bf888af1c7d8ed6f9d3e9850d45f3256194
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-08 15:50:42 -0700

    script with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Reaturn: 0 after printing the function
  */
 int main(void){
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
