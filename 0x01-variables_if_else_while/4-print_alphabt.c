01afa3dd5702dc5c54c7e1c7b0cadb55ec3e8890
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-10 01:51:50 -0700

    script that prints the alphabet in lowercase with an exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: program to convert an alphabet to lowercase
+ */
+
+int main(void)
+{
+	char n = 'A';
+	char l;
+
+	while (n <= 'Z')
+	{
+		if (n == 'Z')
+		{
+			n++;
+			continue;
+		}
+		else if (n == 'E')
+		{
+			n++;
+			continue;
+		}
+		l = tolower(n);
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
