1fcc3181f336e3140182279f976013f2ce18bfcd
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-10 04:34:26 -0700

    script to print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: program to print alphabets in reverse
+ */
+
+int main(void)
+{
+	char n = 'a';
+	char l;
+
+	while (n <= 'z')
+	{
+		putchar(l);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
