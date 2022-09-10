63d133305082504c1a1816dabee8adcab03e6e1b
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-10 05:11:51 -0700

    script to print all numbers to base 10

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: program to print all the numbers of base 16
+ */
+int main(void)
+{
+	char n;
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
