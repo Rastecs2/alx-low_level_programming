f5fa28c1f1db5487683d482b8fe4ca3d16a14974
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-10 05:18:23 -0700

    script that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: program that prints all possible combinations of single-digit numbers
+ */
+
+int main (void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(c);
+
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return 0;
+}
