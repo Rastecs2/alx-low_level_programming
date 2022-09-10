f505c777745171ec94ec9ca3be8435cb22bc52bc
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-09 22:25:21 -0700

    script with if else to identifylast digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,24 +8,24 @@
  */
 int main(void)
 {
-        int n, l;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* if statements to find out if the last digit of n is >5, ==0, <6 */
+	int n, l;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* if statements to find out if the last digit of n is >5, ==0, <6 */
 	l = n % 10;
 	if (l > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, l);
-        }
-        else if (l == 0)
-        {
-                printf("Last digit of %d is %d and is 0\n", n, l);
-        }
-        else if (l < 6 && l != 0)
-        {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-        }
-        return (0);
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else if (l < 6 && l != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
 }
 
