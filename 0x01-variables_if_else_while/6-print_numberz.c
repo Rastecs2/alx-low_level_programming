77ea6b997e176b844176d4d13332d4549e40e910
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-10 03:20:24 -0700

    script that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: program that prints all single digit numbers 0f base 10
+ */
+
+int main(void)
+{
+	int r = '0';
+	
+	while (r < '10')
+	{
+		putchar(r);
+		r++;
+	}
+	putchar("\n");
+	return (0);
+}
