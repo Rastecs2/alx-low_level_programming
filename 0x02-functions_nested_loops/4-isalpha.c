fd6e2c5ec958efc0541e868bc345b627834a51d2
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-14 06:43:10 -0700

    function checking for an alhpabetic character lower or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for aphabetic character
+ *
+ * @c: ASCII value character
+ *
+ * Return: 1 if value stored in c is a letter otherwise returns 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return(1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
