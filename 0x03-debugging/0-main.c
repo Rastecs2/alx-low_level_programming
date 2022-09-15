fba4c795bf04a359bb6642856f4e156b7c83bbb7
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-15 13:29:38 -0700

    function to identify a positive or negative number

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,22 +8,20 @@
  */
 void positive_or_negative(int i)
 {
-	int n;
-
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	i = rand() - RAND_MAX / 2;
 	/* if statements to find out if n is 0, positive or negative */
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf(i, "%d is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf(i, "%d is zero\n");
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		printf(i, "%d is negative\n");
 	}
-	return (0);
+	return (i);
 }
