b3965ffd3765a86123f4615fd17bbbd3710387a7
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-15 13:53:40 -0700

    testing the function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,14 @@
 #include "main.h"
 
 /**
- * positive_or_negative - checks if a number is positive or negative
- * @i: value to be checked
- *
- * Return: ....
+ * main - test the function positive_or_negative
+ * Return: Always 0
  */
-void positive_or_negative(int i)
+int main(void)
 {
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	/* if statements to find out if n is 0, positive or negative */
-	if (i > 0)
-	{
-		printf(i, "%d is positive\n");
-	}
-	else if (n == 0)
-	{
-		printf(i, "%d is zero\n");
-	}
-	else if (n < 0)
-	{
-		printf(i, "%d is negative\n");
-	}
+	int n;
+
+	n=0;
+	positive_or_negative(n);
 	return (0);
 }
