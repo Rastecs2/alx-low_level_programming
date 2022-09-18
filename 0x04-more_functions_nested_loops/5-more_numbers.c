d169df2bc034c2ee15b88c27802f93ebc32f596f
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-18 10:53:36 -0700

    print 0-14 in row of 10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times numbers from 0 to 14
+ *
+ * Return: ....
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	
+	for (k = 1; k <= 10; k++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			_putchar('i');
+		}
+		for (j = '0'; j <= '4'; j++)
+		{
+			_putchar('1');
+			_putchar(j);
+		}
+	}
+}
