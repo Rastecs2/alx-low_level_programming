c32df6cf2d6dca1397102f2949e4db51ac59f514
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-19 13:22:27 -0700

    swap numbers through dereferencing

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers
+ *
+ * @a: first value to swap
+ * @b: second value to swap
+ *
+ * Return: none
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
