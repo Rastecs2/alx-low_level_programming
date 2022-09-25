426e3a5e3ef48080797e0d37c876d1f299e2d5a9
Rastecs2 <erastochoreisanda@gmail.com>
2022-09-25 02:02:56 -0700

    prints a statement without puts or printf and returns 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-
-
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  */
 
 int main(void)
 {
-	fprintf(stderr, " and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	char str1[] = "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
 }
