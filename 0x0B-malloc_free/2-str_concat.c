e33dc986d14728a3f035efd8ccf6d38f6b0ae331
Rastecs2 <erastochoreisanda@gmail.com>
2022-10-05 17:52:40 -0700

    function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,41 +13,36 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-	else if (s2 == NULL)
-	{
+
+	if (s2 == NULL)
 		s2 = "";
-	}
+
 	while (s1[i])
-	{
 		i++;
-	}
+
 	while (s2[j])
-	{
 		j++;
-	}
+
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 
 	if (s == NULL)
-	{
 		return (NULL);
-	}
+
 	j = 0;
 
 	while (k < l)
 	{
 		if (k <= i)
-		{
 			s[k] = s1[k];
-		}
-		else if (k >= i)
+
+		if (k >= i)
 		{
 			s[k] = s2[j];
 			j++;
 		}
+
 		k++;
 	}
 
