99b19bbe0f0ac4486393c26db4befaf2af10d8a2
Rastecs2 <erastochoreisanda@gmail.com>
2022-10-14 08:45:15 -0700

    function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - function that prints a name
+ *
+ * @name: name input
+ * @f: function pointer
+ * Return: ---
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
