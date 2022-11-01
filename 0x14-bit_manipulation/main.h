3183d087c3946a2de75c3581bd03b4cf8d43dc43
Rastecs2 <erastochoreisanda@gmail.com>
2022-11-01 14:06:48 -0700

    function prototypes

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+#ifdef DEBUG /*prints debug statements*/
+#endif
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
